9b77a361146e0a5b26d675e875f58b924c573238
pencilwith-front <gunhosong@welliv.co.kr>
2021-04-06 14:14:09 +0900

    simple zefyr coding complete, 약관 및 프로필(account)페이지 작성 중

--- lib/pages/account.dart
+++ lib/pages/account.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pencilwith/pages/mainpage.dart';
 
 class Account extends StatefulWidget {
   @override
@@ -8,8 +12,60 @@ class Account extends StatefulWidget {
 class _AccountState extends State<Account> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Account'),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        shape: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey.withOpacity(0.5)),
+          //borderRadius: BorderRadius.zero,
+        ),
+        elevation: 0,
+        title: Text(
+          'PROFILE',
+          style: GoogleFonts.getFont('Roboto',
+              fontSize: 18,
+              color: Colors.black,
+              fontStyle: FontStyle.italic,
+              letterSpacing: 3),
+        ),
+      ),
+      body: Column(
+        children: [
+          Spacer(),
+          Text(
+            '재민님 Profile-page 동일화면',
+            textScaleFactor: 1.5,
+          ),
+          Spacer(),
+          GestureDetector(
+            onTap: () {
+              Get.off(MainPage());
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.grey,
+              ),
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              width: double.infinity,
+              height: 60,
+              child: Center(
+                child: Text(
+                  '동의 후 계속',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  textScaleFactor: 1.5,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          )
+        ],
+      ),
     );
   }
 }
